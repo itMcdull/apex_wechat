ebb5c5447ec77a18ca2021b1d987f649bed33316
崔 明辉 <cuis@vip.qq.com>
2021-07-26 15:45:35 +0800

    feat: Add web template.

---
+++ lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/widgets.dart';
+import 'package:mpcore/mpcore.dart';
+
+void main() {
+  runApp(MyApp());
+  MPCore().connectToHostChannel();
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MPApp(
+      title: 'MPFlutter Demo',
+      color: Colors.blue,
+      routes: {
+        '/': (context) => MyHomePage(),
+      },
+      navigatorObservers: [MPCore.getNavigationObserver()],
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MPScaffold(
+      body: Center(
+        child: Container(
+          width: 200,
+          height: 200,
+          color: Colors.green,
+          child: Center(
+            child: Text(
+              'Hello, MPFlutter!',
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
