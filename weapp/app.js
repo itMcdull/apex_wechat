cda546851ceca162490a3a99ff1d0b73f28d160d
PonyCui <cuis@vip.qq.com>
2021-10-15 18:01:03 +0800

    chore: upgrade to mpflutter 0.7.1

--- weapp/app.js
+++ weapp/app.js
@@ -1,21 +1,21 @@
 // app.js
 App({
   onLaunch() {
+    const { MPEnv, Engine, WXApp } = require("./mpdom.min");
+    MPEnv.platformAppInstance = this;
     try {
-      require('./plugins.min');
-    } catch (error) { }
-    const { Engine, WXApp } = require('./mpdom.min');
+      require("./plugins.min");
+    } catch (error) {}
     const engine = new Engine();
     var dev = true;
     if (dev) {
-      engine.initWithDebuggerServerAddr('127.0.0.1:9898');
+      engine.initWithDebuggerServerAddr("127.0.0.1:9898");
+    } else {
+      engine.initWithCodeBlock(Engine.codeBlockWithFile("./main.dart.js"));
     }
-    else {
-      engine.initWithCodeBlock(Engine.codeBlockWithFile('./main.dart.js'));
-    }
-    const app = new WXApp('pages/index/index', engine);
-    global.app = app;
+    const app = new WXApp("pages/index/index", engine);
+    this.app = app;
     engine.start();
   },
-  globalData: {}
-})
+  globalData: {},
+});
